acc6fac7c7abb0df7d9ede7fdfec3fbae16af9b9
Stalkcomrade <stanislavpoznyakof@gmail.com>
2018-09-09 16:25:37 +0200

    edited props name

--- resources/view-calendar.js
+++ resources/view-calendar.js
@@ -1,10 +1,8 @@
 window.CalendarView =  Vue.component('overview', {
 	template: `
-		<month-view v-bind:month="Number(this.month) || ((new Date).getMonth() + 1)" 
-                    v-bind:year="Number(this.year) || (new Date).getFullYear()"
-                    v-bind:maxWebstrates="20" 
+		<month-view v-bind:monthProp="Number(this.month) || ((new Date).getMonth() + 1)" 
+                    v-bind:yearProp="Number(this.year) || (new Date).getFullYear()"
+                    v-bind:maxWebstratesProp="20" 
         />
 	`,
 });
-
-		// <month-view month=8 maxWebstrates=20 />
