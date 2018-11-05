c4795eea139ce90c151032afe5fda41e31771ab5
stalkcomrade <stanislavpoznyakof@gmail.com>
2018-11-05 18:04:37 +0100

    async fetching html in DOM Tree component

--- resources/dom-tree-component.js
+++ resources/dom-tree-component.js
@@ -8,60 +8,11 @@ window.DomTreeComponent = Vue.component('dom-tree', {
   <div class="treeUnique" v-html="finalHtml"></div>
 </div>
 `,
-    // template: `
-    // <div id="tree" class="tree">
-    //    <div v-bind:style="customStyle">
-    //     <h2 v-bind:style="customStyle"> </h2>
-    //     <h3 v-bind:style="customStyle"> </h3>
-    //   </div>
-    // </div>
-    // `,
     data: () => ({
         finalHtml: '',
         htmlObjectReady: false,
-        htmlString: `<!doctype html>
-<html>
-<body>
-<p>Loading Webstrates</p>
-
-<div class="treeUnique" id="tree">
-   <div v-bind:style="customStyle">
-    <h2> </h2>
-    <h3> </h3>
-  </div>
-
-<div class="spinner"></div>
-<style type="text/css" media="screen">
-body {
-	font-family: sans-serif;
-	font-weight: 200;
-	-webkit-font-smoothing: antialiased;
-	text-align: center;
-	margin-top: 15%;
-	animation: fadein 1s;
-}
-@keyframes fadein {
-	0% { opacity: 0; }
-	100% { opacity: 1; }
-}
-.spinner {
-	width: 40px;
-	height: 40px;
-	margin: 0 auto;
-	background-color: #31a46f;
-	animation: rotateplane 1.2s infinite ease-in-out;
-}
-@keyframes rotateplane {
-	0% { transform: perspective(120px) rotateX(0deg) rotateY(0deg); }
-	50% { transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg); }
-	100% { transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg); }
-}
-</style>
-
-</div>
-
-<script src="/webstrates.js?480cb9f456033650d197"></script>
-</body></html>`,
+        htmlObject: '',
+        htmlString: '',
         customStyle: {
             height: "4.5vw",
             boxSizing: "border-box",
@@ -79,91 +30,83 @@ body {
             color: "#fff",
             transition: "all .3s"
         },
-        secondStyle: {
-
-        }
     }),
-    computed: {
-        htmlFromData: function() {
-            const htmlObject = new DOMParser().parseFromString(this.htmlString, "text/html")
-            this.htmlObjectReady = true
-            return {
-                htmlObject
-            }
-        }
-    },
-    watch: {
-        htmlObjectReady() {
-            this.init()
-            console.dir("watched!")
-        },
-    },
+    computed: {},
+    watch: {},
     methods: {
 
-        getLevelNodes: function(node) {
-            return Array.from(node.parentNode.children)
-        },
+        getHtmlsPerSession: async function() {
+                let webpageInitial = await fetch("https://webstrates.cs.au.dk/hungry-cat-75/" + "10/")
+                let htmlResultInitial = await webpageInitial.text()
+                console.dir('html is fetched successfully')
+                return htmlResultInitial
+            },
 
-        getChildIndex: function(node) {
-            return this.getLevelNodes(node).indexOf(node)
-        },
+            getLevelNodes: function(node) {
+                return Array.from(node.parentNode.children)
+            },
 
-        tagNodeName: function(node) {
-            node.innerHTML = node.nodeName + node.innerHTML
-        },
+            getChildIndex: function(node) {
+                return this.getLevelNodes(node).indexOf(node)
+            },
 
-        clearInside: function(node) {
-            Array.from(node.childNodes).forEach(child => {
-                if (child.nodeName === '#text') {
-                    child.remove()
+            tagNodeName: function(node) {
+                node.innerHTML = node.nodeName + node.innerHTML
+            },
 
+            clearInside: function(node) {
+                Array.from(node.childNodes).forEach(child => {
+                    if (child.nodeName === '#text') {
+                        child.remove()
+
+                    }
+                })
+            },
+
+            handleImage: function(node) {
+                if (node.nodeName === 'IMG') {
+                    node.src = ''
+                    node.alt = 'IMG'
                 }
-            })
-        },
+            },
 
-        handleImage: function(node) {
-            if (node.nodeName === 'IMG') {
-                node.src = ''
-                node.alt = 'IMG'
-            }
-        },
+            walk: function(node, cb) {
+                cb(node)
+                if (node.children.length) {
+                    this.walk(node.children[0], cb)
+                }
+                if (node.nextElementSibling) {
+                    this.walk(node.nextElementSibling, cb)
+                }
+            },
 
-        walk: function(node, cb) {
-            cb(node)
-            if (node.children.length) {
-                this.walk(node.children[0], cb)
-            }
-            if (node.nextElementSibling) {
-                this.walk(node.nextElementSibling, cb)
-            }
-        },
-
-        init: function() {
-            console.dir("init starts")
-            var $el = this.htmlFromData.htmlObject.getElementById('tree')
-            window.element = this.htmlFromData.htmlObject.getElementById('tree')
-            this.walk($el.children[0], node => {
-                var levelNodes = this.getLevelNodes(node)
-                var childIndex = this.getChildIndex(node)
-                var width = 90 / levelNodes.length
-                var leftSlice = 100 / levelNodes.length
-                var left = leftSlice * childIndex
-                this.clearInside(node)
-                this.tagNodeName(node)
-                this.handleImage(node)
-                // it is important not to specify color and background in case of :hover usage
-                node.style.cssText += `;
+            init: function() {
+                console.dir("init starts")
+                var $el = this.htmlObject.getElementsByTagName("BODY")[0]
+                this.walk($el.children[0], node => {
+                    var levelNodes = this.getLevelNodes(node)
+                    var childIndex = this.getChildIndex(node)
+                    var width = 90 / levelNodes.length
+                    var leftSlice = 100 / levelNodes.length
+                    var left = leftSlice * childIndex
+                    this.clearInside(node)
+                    this.tagNodeName(node)
+                    this.handleImage(node)
+                    // it is important not to specify color and background in case of :hover usage
+                    node.style.cssText += `;
         width: ${width}%;
         left:  ${left}%;
         line-height: 2.5vw;
       `
-            })
-            this.finalHtml = $el.innerHTML
-        }
+                })
+                this.finalHtml = $el.innerHTML
+            }
     },
     beforeCreate() {},
-    created() {},
-    mounted() {
-        console.dir(this.htmlFromData.htmlObject)
+    async created() {},
+    async mounted() {
+        this.htmlString = await this.getHtmlsPerSession()
+        this.htmlObject = new DOMParser().parseFromString(this.htmlString, "text/html")
+        this.init()
     }
 })
